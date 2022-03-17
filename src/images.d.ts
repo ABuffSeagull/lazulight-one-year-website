6b0c8c811339f6a345c311cdb48fc021e6a27f67
Kerry Wang <kwzeyunwang@gmail.com>
2022-03-17 16:57:23 -0400

    ts-standard action

--- src/images.d.ts
+++ src/images.d.ts
@@ -1,17 +1,17 @@
-declare module "*&imagetools" {
-	const urls: string[];
-	export default urls;
+declare module '*&imagetools' {
+  const urls: string[]
+  export default urls
 }
-declare module "*&imagetools&single" {
-	const url: string;
-	export default url;
+declare module '*&imagetools&single' {
+  const url: string
+  export default url
 }
-declare module "*&imagetools&meta" {
-	const metadata: {
-		src: string;
-		width: number;
-		height: number;
-		format: string;
-	}[];
-	export default metadata;
+declare module '*&imagetools&meta' {
+  const metadata: Array<{
+    src: string
+    width: number
+    height: number
+    format: string
+  }>
+  export default metadata
 }
