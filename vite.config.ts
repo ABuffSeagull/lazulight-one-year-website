00fd2a6e5e9ee4a44685fe46b94648ed8907b8a4
Kerry Wang <kwzeyunwang@gmail.com>
2022-03-17 14:26:59 -0400

    update base path (again)

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import checker from "vite-plugin-checker";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), imagetools(), checker({ typescript: true })],
-	base: '/lazulight-one-year-website-1/'
+	base: '/lazulight-one-year-website/'
 });
