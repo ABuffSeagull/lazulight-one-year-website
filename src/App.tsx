7fce9dbc3e27694f6361cc28db0fc44111d6f036
Ken Lin <kel117@pitt.edu>
2022-03-17 00:35:21 -0400

    Added components for Pomu and Finana

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
-import { Route, Switch, Link } from 'wouter'
-import classes from './App.module.scss'
-import Elira from './Elira'
+import { Route, Switch, Link } from "wouter";
+import classes from "./App.module.scss";
+import Elira from "./pages/Elira";
+import Finana from "./pages/Finana";
+import Pomu from "./pages/Pomu";
 
-export default function App () {
-  return (
-    <div className={classes.container}>
-      <nav>
-        <Link className={classes.link} to='/Finana'>To Finana</Link>
-        <Link className={classes.link} to='/Pomu'>To Pomu</Link>
-        <Link className={classes.link} to='/Elira'>To Elira</Link>
-      </nav>
-      <Switch>
-        <Route path='/Elira'><Elira /></Route>
-        <Route path='/Finana'><div>Finana!</div></Route>
-        <Route path='/Pomu'><div>Pomu!</div></Route>
-        <Route><div>Whomst?</div></Route>
-      </Switch>
-    </div>
-  )
+export default function App() {
+    return (
+        <div className={classes.container}>
+            <nav>
+                <Link className={classes.link} to="/Finana">
+                    To Finana
+                </Link>
+                <Link className={classes.link} to="/Pomu">
+                    To Pomu
+                </Link>
+                <Link className={classes.link} to="/Elira">
+                    To Elira
+                </Link>
+            </nav>
+            <Switch>
+                <Route path="/Elira">
+                    <Elira />
+                </Route>
+                <Route path="/Finana">
+                    <Finana></Finana>
+                </Route>
+                <Route path="/Pomu">
+                    <Pomu></Pomu>
+                </Route>
+                <Route>
+                    <div>Whomst?</div>
+                </Route>
+            </Switch>
+        </div>
+    );
 }
