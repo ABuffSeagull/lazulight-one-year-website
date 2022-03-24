a3ba064a45c8234916e1e99aec536762db60f7e6
Kerry Wang <zwang34@email.wm.edu>
2022-03-24 10:01:38 -0400

    added vite-plugin-compression

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { imagetools } from "vite-imagetools";
 import checker from "vite-plugin-checker";
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), imagetools(), checker({ typescript: true })]
+	plugins: [react(),
+	imagetools(),
+	checker({ typescript: true }),
+	viteCompression(), // Compress files with gzip
+	viteCompression({ algorithm: 'brotliCompress', ext: '.br' }) // Compress files with brotli
+	]
 });
