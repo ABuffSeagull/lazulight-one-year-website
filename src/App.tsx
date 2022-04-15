b6ff62cc9fa4450312a2511121579937f8748078
Matthew Tao <43839811+matthewyingtao@users.noreply.github.com>
2022-04-15 17:58:17 +1200

    lint files

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
-import { Route, Switch } from "wouter";
-import classes from "./App.module.scss";
-import SiteHeader from "./components/SiteHeader";
+import { Route, Switch } from 'wouter'
+import classes from './App.module.scss'
+import SiteHeader from './components/SiteHeader'
 
-import Elira from "./pages/Elira";
-import Finana from "./pages/Finana";
-import ImageExample from "./pages/ImageExample";
-import Pomu from "./pages/Pomu";
+import Elira from './pages/Elira'
+import Finana from './pages/Finana'
+import ImageExample from './pages/ImageExample'
+import Pomu from './pages/Pomu'
 
-export default function App(): JSX.Element {
-	return (
-		<div className={classes.container}>
-			<SiteHeader />
-			<Switch>
-				<Route path="/Elira">
-					<Elira />
-				</Route>
-				<Route path="/Finana">
-					<Finana />
-				</Route>
-				<Route path="/Pomu">
-					<Pomu />
-				</Route>
-				<Route>
-					<div>Whomst?</div>
-					<ImageExample />
-				</Route>
-			</Switch>
-		</div>
-	);
+export default function App (): JSX.Element {
+  return (
+    <div className={classes.container}>
+      <SiteHeader />
+      <Switch>
+        <Route path='/Elira'>
+          <Elira />
+        </Route>
+        <Route path='/Finana'>
+          <Finana />
+        </Route>
+        <Route path='/Pomu'>
+          <Pomu />
+        </Route>
+        <Route>
+          <div>Whomst?</div>
+          <ImageExample />
+        </Route>
+      </Switch>
+    </div>
+  )
 }
