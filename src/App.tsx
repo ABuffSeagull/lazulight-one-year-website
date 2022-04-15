094fb3b09295db36218031f650b2b2af68b9b040
Matthew Tao <43839811+matthewyingtao@users.noreply.github.com>
2022-04-15 17:41:45 +1200

    added SiteHeader component

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,31 @@
-import { Route, Switch, Link } from 'wouter'
-import classes from './App.module.scss'
-import Elira from './pages/Elira'
-import Finana from './pages/Finana'
-import ImageExample from './pages/ImageExample'
-import Pomu from './pages/Pomu'
+import { Route, Switch } from "wouter";
+import classes from "./App.module.scss";
+import SiteHeader from "./components/SiteHeader";
 
-export default function App (): JSX.Element {
-  return (
-    <div className={classes.container}>
-      <nav>
-        <Link className={classes.link} to='/Finana'>
-          To Finana
-        </Link>
-        <Link className={classes.link} to='/Pomu'>
-          To Pomu
-        </Link>
-        <Link className={classes.link} to='/Elira'>
-          To Elira
-        </Link>
-      </nav>
-      <Switch>
-        <Route path='/Elira'>
-          <Elira />
-        </Route>
-        <Route path='/Finana'>
-          <Finana />
-        </Route>
-        <Route path='/Pomu'>
-          <Pomu />
-        </Route>
-        <Route>
-          <div>Whomst?</div>
-          <ImageExample />
-        </Route>
-      </Switch>
-    </div>
-  )
+import Elira from "./pages/Elira";
+import Finana from "./pages/Finana";
+import ImageExample from "./pages/ImageExample";
+import Pomu from "./pages/Pomu";
+
+export default function App(): JSX.Element {
+	return (
+		<div className={classes.container}>
+			<SiteHeader />
+			<Switch>
+				<Route path="/Elira">
+					<Elira />
+				</Route>
+				<Route path="/Finana">
+					<Finana />
+				</Route>
+				<Route path="/Pomu">
+					<Pomu />
+				</Route>
+				<Route>
+					<div>Whomst?</div>
+					<ImageExample />
+				</Route>
+			</Switch>
+		</div>
+	);
 }
---
+++ src/components/SiteHeader.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { Link } from "wouter";
+import Image from "./Image";
+import classes from "./SiteHeader.module.scss";
+
+import finanaIconDefault from "../assets/menu_icons/Menu-Finana-Default.webp";
+import finanaIconActive from "../assets/menu_icons/Menu-Finana-Active.webp";
+
+import pomuIconDefault from "../assets/menu_icons/Menu-Pomu-Default.webp";
+import pomuIconActive from "../assets/menu_icons/Menu-Pomu-Active.webp";
+
+import eliraIconDefault from "../assets/menu_icons/Menu-Elira-Default.webp";
+import eliraIconActive from "../assets/menu_icons/Menu-Elira-Active.webp";
+
+import projectsIconDefault from "../assets/menu_icons/Menu-Bow-Default.webp";
+import projectsIconActive from "../assets/menu_icons/Menu-Bow-Active.webp";
+
+import submissionsIconDefault from "../assets/menu_icons/Menu-EliraTiara-Default.webp";
+import submissionsIconActive from "../assets/menu_icons/Menu-EliraTiara-Active.webp";
+
+import VNIconDefault from "../assets/menu_icons/Menu-FinanaCrown-Default.webp";
+import VNIconActive from "../assets/menu_icons/Menu-FinanaCrown-Active.webp";
+
+export default function SiteHeader() {
+	const navIcons = [
+		{
+			name: "Home",
+			icon: projectsIconDefault,
+			iconActive: projectsIconActive,
+			path: "/"
+		},
+		{
+			name: "Pomu",
+			icon: pomuIconDefault,
+			iconActive: pomuIconActive,
+			path: "/pomu"
+		},
+		{
+			name: "Elira",
+			icon: eliraIconDefault,
+			iconActive: eliraIconActive,
+			path: "/elira"
+		},
+		{
+			name: "Finana",
+			icon: finanaIconDefault,
+			iconActive: finanaIconActive,
+			path: "/finana"
+		},
+		{
+			name: "Submissions",
+			icon: submissionsIconDefault,
+			iconActive: submissionsIconActive,
+			path: "/finana"
+		},
+		{
+			name: "VN",
+			icon: VNIconDefault,
+			iconActive: VNIconActive,
+			path: "/finana"
+		}
+	];
+
+	return (
+		<header className={classes.header}>
+			{navIcons.map((linkData) => (
+				<Link className={classes.navLink} to={linkData.path}>
+					<div className={classes.iconWrapper}>
+						<Image
+							className={[classes.icon, classes.defaultIcon].join(" ")}
+							src={linkData.icon}
+						/>
+						<Image
+							className={[classes.icon, classes.activeIcon].join(" ")}
+							src={linkData.iconActive}
+						/>
+					</div>
+					<span>{linkData.name}</span>
+				</Link>
+			))}
+		</header>
+	);
+}
