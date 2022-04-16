37fd979f0297b8a03686415f4bb914adadcb3c37
ABuffSeagull <reecevanatta@hey.com>
2022-04-15 21:10:28 -0400

    Enhance the image wrapper
    Now it can correctly sort the formats in *approximately* order of
    efficiency. It can also deal with specifying multiple widths and sizes
    for that true responsive action.

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,34 +1,51 @@
-interface ImageToolProps {
+interface ImageToolProps{
   src: metadata[] | string
+  sizes?: string
   className?: string
   alt?: string
   width?: string
   height?: string
 }
 
-export default function Image (props: ImageToolProps): JSX.Element {
+const formats = ['avif', 'heic', 'heif', 'webp', 'jpg', 'jpeg', 'png']
+export default function Picture (props: ImageToolProps): JSX.Element {
   if (typeof props.src === 'string') {
-    return (
-      <img className={props.className} src={props.src} width={props.width} height={props.height} alt={props.alt} />
-    )
-  } else {
-    const [fallback, ...sources] = props.src
-    return (
-      <picture className={props.className}>
-        {sources.map((metadata) => (
-          <source
-            key={metadata.src}
-            type={`image/${metadata.format}`}
-            srcSet={metadata.src}
-          />
-        ))}
-        <img
-          src={fallback.src}
-          width={props.width}
-          height={props.height}
-          alt={props.alt}
-        />
-      </picture>
-    )
+    // I have to explicitly pass src to make TypeScript happy
+    return <img {...props} src={props.src} />
   }
+
+  const { src: metadata, ...passthroughProps } = props
+  const groupedFormats: Record<string, metadata[]> = {}
+  let maxWidth = 0
+  let maxHeight = 0
+  for (const data of metadata) {
+    maxWidth = Math.max(maxWidth, data.width)
+    maxHeight = Math.max(maxHeight, data.height)
+    groupedFormats[data.format] ??= []
+    // We literally just init it the line before this
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    groupedFormats[data.format]!.push(data)
+  }
+
+  const formatEntries = Object.entries(groupedFormats)
+  formatEntries.sort(([firstFormat], [secondFormat]) => formats.indexOf(firstFormat) - formats.indexOf(secondFormat))
+
+  const sources = []
+  for (const [format, metaArray] of formatEntries.slice(0, -1)) {
+    sources.push(<source key={format} type={`image/${format}`} srcSet={makeSrcSet(metaArray)} />)
+  }
+
+  // If only typescript was smart
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const [, fallbackMetadata] = formatEntries[formatEntries.length - 1]!
+  return (
+    <picture className={passthroughProps.className ?? ''}>
+      {sources}
+      <img width={maxWidth} height={maxHeight} srcSet={makeSrcSet(fallbackMetadata)} {...passthroughProps} />
+    </picture>
+  )
+}
+
+function makeSrcSet (metadata: metadata[]): string {
+  return metadata.map(({ src, width }) => `${src} ${width}w`).join(',')
 }
