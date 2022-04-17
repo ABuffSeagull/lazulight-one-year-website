a7a69a8d935b1f3c5db3707c83fb7656143dae3f
Brandon Johns <69143173+Brandon-Johns@users.noreply.github.com>
2022-04-17 23:45:25 +1000

    animated BG - elira - rough

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -2,6 +2,12 @@ import { Link } from 'wouter'
 import React from 'react'
 import './PageWrapper.scss'
 import Image from './Image'
+import EliraCloud1 from '../assets/BG/Elira-BG-Clould1.webp'
+import EliraCloud2 from '../assets/BG/Elira-BG-Clould2.webp'
+import EliraCloud3 from '../assets/BG/Elira-BG-Clould3.webp'
+import EliraCloud4 from '../assets/BG/Elira-BG-Clould4.webp'
+import EliraStar1 from '../assets/BG/Elira-BG-Star1.webp'
+// import EliraStar2 from '../assets/BG/Elira-BG-Star2.webp' // Individual stars looks better I think
 import PomuCoinGold from '../assets/BG/Pomu-BG-PomuCoinGold.svg'
 import PomuCoinSilver from '../assets/BG/Pomu-BG-PomuCoinSilver.svg'
 
@@ -11,12 +17,31 @@ interface Props {
 }
 
 export function PageWrapper (props: Props): JSX.Element {
+  // (Brandon) These constants must match values in class 'page-bg-animation-${props.page}-element'
+  const eliraCloudCount: number = 4 * 4 // Should be multiple of 4
+  const eliraStarCount: number = 60
+  const pomucoinCount: number = 2 * 30 // Should be multiple of 2
+
   return (
     <div className={`page-bg-static-${props.page} page-bg-animation-${props.page}-container`}>
       {props.page === 'pomu' && (
         <div>
-          {Array.from({ length: 30 }, i => <Image className='page-bg-animation-pomu-element' src={PomuCoinGold} />)}
-          {Array.from({ length: 30 }, i => <Image className='page-bg-animation-pomu-element' src={PomuCoinSilver} />)}
+          {Array.from({ length: pomucoinCount / 2 }, i => <Image className='page-bg-animation-pomu-coin' src={PomuCoinGold} />)}
+          {Array.from({ length: pomucoinCount / 2 }, i => <Image className='page-bg-animation-pomu-coin' src={PomuCoinSilver} />)}
+        </div>
+      )}
+      {props.page === 'elira' && (
+        <div>
+          {/* Separate div's because of the nth-child selector */}
+          <div>
+            {Array.from({ length: eliraCloudCount / 4 }, i => <Image className='page-bg-animation-elira-cloud' src={EliraCloud1} />)}
+            {Array.from({ length: eliraCloudCount / 4 }, i => <Image className='page-bg-animation-elira-cloud' src={EliraCloud2} />)}
+            {Array.from({ length: eliraCloudCount / 4 }, i => <Image className='page-bg-animation-elira-cloud' src={EliraCloud3} />)}
+            {Array.from({ length: eliraCloudCount / 4 }, i => <Image className='page-bg-animation-elira-cloud' src={EliraCloud4} />)}
+          </div>
+          <div>
+            {Array.from({ length: eliraStarCount }, i => <Image className='page-bg-animation-elira-star' src={EliraStar1} />)}
+          </div>
         </div>
       )}
 
