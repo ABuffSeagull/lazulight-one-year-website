ddc1eaad504dffe9a806f0eed46915b8de55054e
Kerry Wang <kwzeyunwang@gmail.com>
2022-04-18 09:51:57 -0400

    lint

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -3,46 +3,46 @@ import { useLocation } from 'wouter'
 import classes from './SiteFooter.module.scss'
 
 const footerText = [
-    'Some web team credit',
-    'test paragraph',
-    '',
-    'some fan site disclaimer',
-    ''
+  'Some web team credit',
+  'test paragraph',
+  '',
+  'some fan site disclaimer',
+  ''
 ]
 
-  
-export default function SiteFooter () {
-    const location = useLocation()
-    const [firstName, setFirstName] = useState(window.location.pathname)
-    
-    useEffect(() => {
-        setFirstName(window.location.pathname)
-    }, [location])
+export default function SiteFooter (): JSX.Element {
+  const location = useLocation()
+  const [firstName, setFirstName] = useState(window.location.pathname)
 
-    const firstNameLower = firstName.toLowerCase().split('/')[1]
-    let footerBorder
-    switch(firstNameLower) {
-        case 'elira':
-            footerBorder = classes.eliraFooterBorder
-            break;
-        case 'pomu':
-            footerBorder = classes.pomuFooterBorder
-            break;
-        case 'finana':
-            footerBorder = classes.finanaFooterBorder
-            break;
-        default:
-            footerBorder = classes.defaultFooterBorder // TODO: define default footer border colour
-    }
-                
-    return (
-        <>
-          <footer className={[classes.footer, footerBorder].join(' ')}>
-                {footerText.map(paragraph =>
-                <p>{paragraph}&nbsp;</p>
-              )}
-            </footer>
+  useEffect(() => {
+    setFirstName(window.location.pathname)
+  }, [location])
 
-        </>
-      )
+  const firstNameLower = firstName.toLowerCase().split('/')[1]
+  let footerBorder
+  switch (firstNameLower) {
+    case 'elira':
+      footerBorder = classes.eliraFooterBorder
+      break
+    case 'pomu':
+      footerBorder = classes.pomuFooterBorder
+      break
+    case 'finana':
+      footerBorder = classes.finanaFooterBorder
+      break
+    default:
+      footerBorder = classes.defaultFooterBorder // TODO: define default footer border colour
+  }
+
+  return (
+    <>
+      <footer className={[classes.footer, footerBorder].join(' ')}>
+        {footerText.map((paragraph, index) =>
+          // OK to use index as key as long as list is static
+          <p key={index}>{paragraph}&nbsp;</p>
+        )}
+      </footer>
+
+    </>
+  )
 }
