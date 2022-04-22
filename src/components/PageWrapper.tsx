5f9bcf49a322f4c3d03c70540a449260a029f6e5
Matthew Tao <43839811+matthewyingtao@users.noreply.github.com>
2022-04-22 21:21:53 +1200

    fix overlap

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -29,60 +29,109 @@ export function PageWrapper (props: Props): JSX.Element {
   const [AnimationOn, toggleAnimationState] = React.useState(true)
 
   return (
-    <div className={`container page-bg-static-${props.page} page-bg-animation-${props.page}`}>
-      <div className={`page-bg-animation-${props.page}-container ${AnimationOn ? `page-bg-animation-${props.page}-container-on` : `page-bg-animation-${props.page}-container-off`}`}>
+    <div
+      className={`container page-bg-static-${props.page} page-bg-animation-${props.page}`}
+    >
+      {/* TODO: Find somewhere to put toggleAnimation button in menu */}
+      <SiteHeader />
+      <div
+        className={`page-bg-animation-${props.page}-container ${
+          AnimationOn
+            ? `page-bg-animation-${props.page}-container-on`
+            : `page-bg-animation-${props.page}-container-off`
+        }`}
+      >
         {props.page === 'pomu' && (
           <div>
-            {Array.from({ length: pomuCoinCount / 2 }, (i: number) =>
-              <Image className='page-bg-animation-pomu-coin' src={PomuCoinGold} />)}
-            {Array.from({ length: pomuCoinCount / 2 }, (i: number) =>
-              <Image className='page-bg-animation-pomu-coin' src={PomuCoinSilver} />)}
+            {Array.from({ length: pomuCoinCount / 2 }, (i: number) => (
+              <Image
+                className='page-bg-animation-pomu-coin'
+                src={PomuCoinGold}
+              />
+            ))}
+            {Array.from({ length: pomuCoinCount / 2 }, (i: number) => (
+              <Image
+                className='page-bg-animation-pomu-coin'
+                src={PomuCoinSilver}
+              />
+            ))}
           </div>
         )}
         {props.page === 'elira' && (
           <div>
             {/* Separate div's because of the nth-child selector */}
             <div>
-              {Array.from({ length: eliraCloudCount / 4 }, (i: number) =>
-                <Image className='page-bg-animation-elira-cloud' src={EliraCloud1} />)}
-              {Array.from({ length: eliraCloudCount / 4 }, (i: number) =>
-                <Image className='page-bg-animation-elira-cloud' src={EliraCloud2} />)}
-              {Array.from({ length: eliraCloudCount / 4 }, (i: number) =>
-                <Image className='page-bg-animation-elira-cloud' src={EliraCloud3} />)}
-              {Array.from({ length: eliraCloudCount / 4 }, (i: number) =>
-                <Image className='page-bg-animation-elira-cloud' src={EliraCloud4} />)}
+              {Array.from({ length: eliraCloudCount / 4 }, (i: number) => (
+                <Image
+                  className='page-bg-animation-elira-cloud'
+                  src={EliraCloud1}
+                />
+              ))}
+              {Array.from({ length: eliraCloudCount / 4 }, (i: number) => (
+                <Image
+                  className='page-bg-animation-elira-cloud'
+                  src={EliraCloud2}
+                />
+              ))}
+              {Array.from({ length: eliraCloudCount / 4 }, (i: number) => (
+                <Image
+                  className='page-bg-animation-elira-cloud'
+                  src={EliraCloud3}
+                />
+              ))}
+              {Array.from({ length: eliraCloudCount / 4 }, (i: number) => (
+                <Image
+                  className='page-bg-animation-elira-cloud'
+                  src={EliraCloud4}
+                />
+              ))}
             </div>
             <div>
-              {Array.from({ length: eliraStarCount }, (i: number) =>
-                <Image className='page-bg-animation-elira-star' src={EliraStar1} />)}
+              {Array.from({ length: eliraStarCount }, (i: number) => (
+                <Image
+                  className='page-bg-animation-elira-star'
+                  src={EliraStar1}
+                />
+              ))}
             </div>
           </div>
         )}
         {props.page === 'finana' && (
           <div>
             {/* Separate div's because of the nth-child selector */}
-            <div>{Array.from({ length: finanaBubbleCount }, (i: number) =>
-              <Image className='page-bg-animation-finana-bubble' src={FinanaBubble1} />)}
+            <div>
+              {Array.from({ length: finanaBubbleCount }, (i: number) => (
+                <Image
+                  className='page-bg-animation-finana-bubble'
+                  src={FinanaBubble1}
+                />
+              ))}
             </div>
-            <div>{Array.from({ length: finanaFishCount }, (i: number) =>
-              <Image className='page-bg-animation-finana-fish' src={FinanaFish} />)}
+            <div>
+              {Array.from({ length: finanaFishCount }, (i: number) => (
+                <Image
+                  className='page-bg-animation-finana-fish'
+                  src={FinanaFish}
+                />
+              ))}
             </div>
           </div>
         )}
       </div>
 
-      {/* TODO: Find somewhere to put toggleAnimation button in menu */}
-      <SiteHeader />
-      <button onClick={() => { toggleAnimationState(state => !state) }} className='animation-toggle'>
+      <button
+        onClick={() => {
+          toggleAnimationState((state) => !state)
+        }}
+        className='animation-toggle'
+      >
         {AnimationOn ? 'Animation On' : 'Animation Off'}
       </button>
 
       {/* This is the body of the page */}
       {props.children}
 
-      <SiteFooter
-        page={props.page}
-      />
+      <SiteFooter page={props.page} />
 
       {/* Scripts for background animation */}
       {/* See source in: https://codepen.io/matthewyingtao/pen/mdqoZPg */}
