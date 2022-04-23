fd38527d52b1917e475965974a59b2cf0f0a418a
Matthew Tao <43839811+matthewyingtao@users.noreply.github.com>
2022-04-23 14:04:23 +1200

    made corners smaller

--- src/components/MessageBoxesLayout.tsx
+++ src/components/MessageBoxesLayout.tsx
@@ -1,132 +1,193 @@
-import React from 'react'
-import './TalentLayout.scss'
-import Image from './Image'
-import LazulightCorner from '../assets/Corners/Lazulight-MsgCorner.webp' // TODO: Requires asset
-import FinanaCorner from '../assets/Corners/Finana-MsgCorner.webp'
-import EliraCorner from '../assets/Corners/Elira-MsgCorner.webp'
-import PomuCorner from '../assets/Corners/Pomu-MsgCorner.webp'
-import * as messageListImport from '../assets/messageList.json'
+import React from 'react';
+import './TalentLayout.scss';
+import Image from './Image';
+import LazulightCorner from '../assets/Corners/Lazulight-MsgCorner.webp'; // TODO: Requires asset
+import FinanaCorner from '../assets/Corners/Finana-MsgCorner.webp';
+import EliraCorner from '../assets/Corners/Elira-MsgCorner.webp';
+import PomuCorner from '../assets/Corners/Pomu-MsgCorner.webp';
+import * as messageListImport from '../assets/messageList.json';
 
 interface MessageRaw {
-  name: string
-  soical_handle: string
-  soical_url: string
-  msg_lazulight: string
-  msg_elira: string
-  msg_pomu: string
-  msg_finana: string
-  art_lazulight: string
-  art_elira: string
-  art_pomu: string
-  art_finana: string
+  name: string;
+  soical_handle: string;
+  soical_url: string;
+  msg_lazulight: string;
+  msg_elira: string;
+  msg_pomu: string;
+  msg_finana: string;
+  art_lazulight: string;
+  art_elira: string;
+  art_pomu: string;
+  art_finana: string;
 }
 
 interface Message {
-  name: string
-  soical_handle: string
-  soical_url: string
-  msg: string
-  art: string
-  corner: string
-  border: string
+  name: string;
+  soical_handle: string;
+  soical_url: string;
+  msg: string;
+  art: string;
+  corner: string;
+  border: string;
 }
 
 interface Props {
-  enableLazulight: boolean
-  enableElira: boolean
-  enablePomu: boolean
-  enableFinana: boolean
-  enableFolding: boolean
-  enableArt: boolean
-  heading: string
-  page: string
+  enableLazulight: boolean;
+  enableElira: boolean;
+  enablePomu: boolean;
+  enableFinana: boolean;
+  enableFolding: boolean;
+  enableArt: boolean;
+  heading: string;
+  page: string;
 }
 
 // Shuffle algorithm: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
 // Implemented by Ilya Kantor: https://javascript.info/task/shuffle https://javascript.info/terms
-function shuffleMessages (array: Message[]): boolean {
+function shuffleMessages(array: Message[]): boolean {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]]
+    [array[i], array[j]] = [array[j], array[i]];
   }
-  return true
+  return true;
 }
 
-function filterMessage (enable: boolean, enableArt: boolean, msg: string, art: string): boolean {
+function filterMessage(
+  enable: boolean,
+  enableArt: boolean,
+  msg: string,
+  art: string
+): boolean {
   // Msg mode: only allow if user has not submitted art
   // Art mode: allow if user has submitted art
-  return enable && ((!enableArt && msg !== '' && art === '') || (enableArt && art !== ''))
+  return (
+    enable &&
+    ((!enableArt && msg !== '' && art === '') || (enableArt && art !== ''))
+  );
 }
 
-export default function MessageBoxesLayout (props: Props): JSX.Element {
-  const [showAllMessages, setShowAllMessages] = React.useState(!props.enableFolding)
-  const messageListRaw = messageListImport.all as MessageRaw[]
+export default function MessageBoxesLayout(props: Props): JSX.Element {
+  const [showAllMessages, setShowAllMessages] = React.useState(
+    !props.enableFolding
+  );
+  const messageListRaw = messageListImport.all as MessageRaw[];
 
   // Extract all messages from JSON file, filter by enabled type, format, random shuffle, then output
   const renderMessages = (): Message[] => {
-    const Messages: Message[] = []
+    const Messages: Message[] = [];
     messageListRaw.forEach((msg: MessageRaw): void => {
-      if (filterMessage(props.enableLazulight, props.enableArt, msg.msg_lazulight, msg.art_lazulight)) {
+      if (
+        filterMessage(
+          props.enableLazulight,
+          props.enableArt,
+          msg.msg_lazulight,
+          msg.art_lazulight
+        )
+      ) {
         Messages.push({
           name: msg.name,
           soical_handle: msg.soical_handle,
           soical_url: msg.soical_url,
           msg: msg.msg_lazulight,
-          art: (props.enableArt ? msg.art_lazulight : ''),
+          art: props.enableArt ? msg.art_lazulight : '',
           corner: LazulightCorner,
-          border: 'lazulight'
-        })
+          border: 'lazulight',
+        });
       }
-      if (filterMessage(props.enableElira, props.enableArt, msg.msg_elira, msg.art_elira)) {
+      if (
+        filterMessage(
+          props.enableElira,
+          props.enableArt,
+          msg.msg_elira,
+          msg.art_elira
+        )
+      ) {
         Messages.push({
           name: msg.name,
           soical_handle: msg.soical_handle,
           soical_url: msg.soical_url,
           msg: msg.msg_elira,
-          art: (props.enableArt ? msg.art_elira : ''),
+          art: props.enableArt ? msg.art_elira : '',
           corner: EliraCorner,
-          border: 'elira'
-        })
+          border: 'elira',
+        });
       }
-      if (filterMessage(props.enablePomu, props.enableArt, msg.msg_pomu, msg.art_pomu)) {
+      if (
+        filterMessage(
+          props.enablePomu,
+          props.enableArt,
+          msg.msg_pomu,
+          msg.art_pomu
+        )
+      ) {
         Messages.push({
           name: msg.name,
           soical_handle: msg.soical_handle,
           soical_url: msg.soical_url,
           msg: msg.msg_pomu,
-          art: (props.enableArt ? msg.art_pomu : ''),
+          art: props.enableArt ? msg.art_pomu : '',
           corner: PomuCorner,
-          border: 'pomu'
-        })
+          border: 'pomu',
+        });
       }
-      if (filterMessage(props.enableFinana, props.enableArt, msg.msg_finana, msg.art_finana)) {
+      if (
+        filterMessage(
+          props.enableFinana,
+          props.enableArt,
+          msg.msg_finana,
+          msg.art_finana
+        )
+      ) {
         Messages.push({
           name: msg.name,
           soical_handle: msg.soical_handle,
           soical_url: msg.soical_url,
           msg: msg.msg_finana,
-          art: (props.enableArt ? msg.art_finana : ''),
+          art: props.enableArt ? msg.art_finana : '',
           corner: FinanaCorner,
-          border: 'finana'
-        })
+          border: 'finana',
+        });
       }
-    })
-    shuffleMessages(Messages)
-    return Messages
-  }
+    });
+    shuffleMessages(Messages);
+    return Messages;
+  };
 
   return (
-    <div className='fan-submissions-container'>
-      <h3 className={`${props.page}-border`}>{props.heading}</h3>
-      <div className={`${showAllMessages ? 'fan-submissions-box-expanded' : 'fan-submissions-box-faded'}`}>
-        <div className='fan-submissions-box'>
+    <div className="fan-submissions-container">
+      <h3 className="fan-submissions-heading">{props.heading}</h3>
+      <div
+        className={`${
+          showAllMessages
+            ? 'fan-submissions-box-expanded'
+            : 'fan-submissions-box-faded'
+        }`}
+      >
+        <div className="fan-submissions-box">
           {renderMessages().map((message, idx) => (
-            <div key={'msg' + idx.toString()} className='fan-submissions-submission'>
-              <Image className='fan-submissions-corner' src={message.corner} />
-              <div className={`fan-submissions-message ${message.border}-border`}>
-                <h4>{message.soical_url === '' ? message.name : (<a href={message.soical_url}>{message.name}</a>)}</h4>
-                {message.msg !== '' && (<p>{message.msg}</p>)}
-                {message.art !== '' && (<Image className='fan-submissions-fanarti' src={message.art} alt='' />)}
+            <div
+              key={'msg' + idx.toString()}
+              className="fan-submissions-submission"
+            >
+              <Image className="fan-submissions-corner" src={message.corner} />
+              <div
+                className={`fan-submissions-message ${message.border}-border`}
+              >
+                <h4>
+                  {message.soical_url === '' ? (
+                    message.name
+                  ) : (
+                    <a href={message.soical_url}>{message.name}</a>
+                  )}
+                </h4>
+                {message.msg !== '' && <p>{message.msg}</p>}
+                {message.art !== '' && (
+                  <Image
+                    className="fan-submissions-fanarti"
+                    src={message.art}
+                    alt=""
+                  />
+                )}
               </div>
             </div>
           ))}
@@ -134,12 +195,14 @@ export default function MessageBoxesLayout (props: Props): JSX.Element {
       </div>
       {!showAllMessages && (
         <button
-          onClick={() => { setShowAllMessages(true) }}
+          onClick={() => {
+            setShowAllMessages(true);
+          }}
           className={`${props.page}-border`}
         >
           view all
         </button>
       )}
     </div>
-  )
+  );
 }
