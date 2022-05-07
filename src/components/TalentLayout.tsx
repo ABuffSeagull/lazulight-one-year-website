272beb942e0ffd640a3a6bd7ac4518f228e01295
Matthew Tao <43839811+matthewyingtao@users.noreply.github.com>
2022-05-07 22:00:25 +1200

    applied lint rules

--- src/components/TalentLayout.tsx
+++ src/components/TalentLayout.tsx
@@ -1,69 +1,69 @@
-import React, { useRef } from 'react';
-import './TalentLayout.scss';
-import Image from './Image';
-import MessageBoxesLayout from './MessageBoxesLayout';
-import MascotAnimation from './MascotAnimation';
+import React, { useRef } from 'react'
+import './TalentLayout.scss'
+import Image from './Image'
+import MessageBoxesLayout from './MessageBoxesLayout'
+import MascotAnimation from './MascotAnimation'
 
 // TODO(abuffseagull) 2022-03-17: need to remove this
 // Probably just generate some random ids on the messages at load time
 /* eslint-disable react/jsx-key */
 
 interface Props {
-  name: string;
-  artBoardHeading: string;
-  artBoardText: string;
-  artBoard: string;
-  frame: string;
-  animation: string;
-  corner: string;
-  portrait: string | metadata[];
-  info: string[];
-  youtube: string;
-  twitter: string;
-  discord: string;
+  name: string
+  artBoardHeading: string
+  artBoardText: string
+  artBoard: string
+  frame: string
+  animation: string
+  corner: string
+  portrait: string | metadata[]
+  info: string[]
+  youtube: string
+  twitter: string
+  discord: string
 }
 
-function TalentLayout(props: Props): JSX.Element {
-  const firstName = props.name.split(' ')[0];
-  const firstNameLower = firstName.toLowerCase();
+function TalentLayout (props: Props): JSX.Element {
+  const firstName = props.name.split(' ')[0]
+  const firstNameLower = firstName.toLowerCase()
 
-  const vid0 = useRef<HTMLVideoElement>(null);
-  const vid1 = useRef<HTMLVideoElement>(null);
-  const vid2 = useRef<HTMLVideoElement>(null);
-  const vid3 = useRef<HTMLVideoElement>(null);
-  const vid0v = useRef<HTMLVideoElement>(null);
-  const vid1v = useRef<HTMLVideoElement>(null);
+  const vid0 = useRef<HTMLVideoElement>(null)
+  const vid1 = useRef<HTMLVideoElement>(null)
+  const vid2 = useRef<HTMLVideoElement>(null)
+  const vid3 = useRef<HTMLVideoElement>(null)
+  const vid0v = useRef<HTMLVideoElement>(null)
+  const vid1v = useRef<HTMLVideoElement>(null)
 
   return (
-    <div className="talent-layout-container">
-      <div className="talent-profile-container">
-        <div className="talent-picture-box">
+    <div className='talent-layout-container'>
+      <div className='talent-profile-container'>
+        <div className='talent-picture-box'>
           <Image src={props.portrait} />
           {/* (Brandon): Number of images is hardcoded into 'talent-picture-box' */}
           <MascotAnimation id={vid0v} delay={0} src={props.animation} />
           <MascotAnimation id={vid1v} delay={100} src={props.animation} />
         </div>
-        <div className="talent-info-container">
-          <div className="talent-text-container">
-            <Image className="talent-corner" src={props.frame} />
+        <div className='talent-info-container'>
+          <div className='talent-text-container'>
+            <Image className='talent-corner' src={props.frame} />
             <div className={`talent-text-inner ${firstNameLower}-border`}>
               <h2>{props.name}</h2>
               {props.info.map((paragraph, idx) => (
                 <p key={idx}>{paragraph}&nbsp;</p>
               ))}
-              <div className="talent-links-container">
+              <div className='talent-links-container'>
                 <a href={props.youtube}>{firstName}'s Youtube</a>
                 <a href={props.twitter}>{firstName}'s Twitter</a>
                 <a href={props.discord}> Fan Discord</a>
               </div>
             </div>
           </div>
-          <div className="talent-animation-container hide-on-tablet">
+          <div className='talent-animation-container hide-on-tablet'>
             {/* (Brandon): Number of images and class of last image is hardcoded into 'talent-animation-container' */}
             <MascotAnimation id={vid0} delay={0} src={props.animation} />
             <MascotAnimation id={vid1} delay={50} src={props.animation} />
             <MascotAnimation id={vid2} delay={100} src={props.animation} />
-            <div className="hide-on-tablet">
+            <div className='hide-on-tablet'>
               <MascotAnimation id={vid3} delay={150} src={props.animation} />
             </div>
           </div>
@@ -71,12 +71,12 @@ function TalentLayout(props: Props): JSX.Element {
       </div>
 
       {/* Art Board */}
-      <div className="talent-text-container">
-        <Image className="talent-corner" src={props.corner} />
+      <div className='talent-text-container'>
+        <Image className='talent-corner' src={props.corner} />
         <div className={`talent-text-inner ${firstNameLower}-border`}>
-          <h3 className="text-xl">{props.artBoardHeading}</h3>
+          <h3 className='text-xl'>{props.artBoardHeading}</h3>
           <p>{props.artBoardText}</p>
-          <Image className="artboard-image" src={props.artBoard} />
+          <Image className='artboard-image' src={props.artBoard} />
         </div>
       </div>
 
@@ -104,7 +104,7 @@ function TalentLayout(props: Props): JSX.Element {
         page={firstNameLower}
       />
     </div>
-  );
+  )
 }
 
-export default TalentLayout;
+export default TalentLayout
