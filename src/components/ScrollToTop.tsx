687071a9859628b99eec6568e30fc1e4d0cc972f
Brandon Johns <69143173+Brandon-Johns@users.noreply.github.com>
2022-05-10 16:52:57 +1000

    scroll -> instant

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -5,7 +5,7 @@ export default function ScrollToTop (): null {
   const [pathname] = useLocation()
 
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'smooth' })
+    window.scrollTo({ top: 0, behavior: 'auto' })
   }, [pathname])
 
   return null
