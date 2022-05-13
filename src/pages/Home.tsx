0769998877f91077f9ec667c734535d730a733f5
Matthew Tao <43839811+matthewyingtao@users.noreply.github.com>
2022-05-13 23:07:51 +1200

    update home page header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,45 +1,47 @@
-import React from 'react'
-import { Link } from 'wouter'
-import '../components/TalentLayout.scss'
-import '../components/HomePage.scss'
-import Image from '../components/Image'
-import MessageBoxesLayout from '../components/MessageBoxesLayout'
-import EliraImage from '../assets/Portraits/Elira-Portrait_cf50.webp?format=png;avif;webp&imagetools&meta'
-import FinanaImage from '../assets/Portraits/Finana-Portrait_cf50.webp?format=png;avif;webp&imagetools&meta'
-import PomuImage from '../assets/Portraits/Pomu-Portrait_cf50.webp?format=png;avif;webp&imagetools&meta'
+import React from 'react';
+import { Link } from 'wouter';
+import '../components/TalentLayout.scss';
+import '../components/HomePage.scss';
+import Image from '../components/Image';
+import MessageBoxesLayout from '../components/MessageBoxesLayout';
+import EliraImage from '../assets/Portraits/Elira-Portrait_cf50.webp?format=png;avif;webp&imagetools&meta';
+import FinanaImage from '../assets/Portraits/Finana-Portrait_cf50.webp?format=png;avif;webp&imagetools&meta';
+import PomuImage from '../assets/Portraits/Pomu-Portrait_cf50.webp?format=png;avif;webp&imagetools&meta';
 
-export default function Home (): JSX.Element {
+export default function Home(): JSX.Element {
   const navIcons = [
     {
       name: 'Elira',
       path: '/elira',
-      image: EliraImage
+      image: EliraImage,
     },
     {
       name: 'Finana',
       path: '/finana',
-      image: FinanaImage
+      image: FinanaImage,
     },
     {
       name: 'Pomu',
       path: '/pomu',
-      image: PomuImage
-    }
-  ]
+      image: PomuImage,
+    },
+  ];
 
   return (
-    <div className='talent-layout-container'>
+    <div className="talent-layout-container">
       {/* Opening Message */}
-      <h1 className='home-heading'>
-        LazuLight 1 Year
-        <br />
-        and Counting!
-      </h1>
+      <div>
+        <h1 className="home-heading">
+          <span>Lazu</span>
+          <span>Light</span> <span>1 Year</span>
+        </h1>
+        <h2 className="home-subheading">and Counting!</h2>
+      </div>
 
       {/* Links to liver pages */}
-      <div className='home-liver-container'>
+      <div className="home-liver-container">
         {navIcons.map((linkData, i) => (
-          <div className='home-liver-liver' key={`LiverLink${linkData.name}`}>
+          <div className="home-liver-liver" key={`LiverLink${linkData.name}`}>
             <Link to={linkData.path}>
               <Image src={linkData.image} />
               <h3>{linkData.name}</h3>
@@ -56,8 +58,8 @@ export default function Home (): JSX.Element {
         enableFinana={false}
         enableFolding
         enableArt={false}
-        heading='Messages To LazuLight'
-        page='home'
+        heading="Messages To LazuLight"
+        page="home"
       />
 
       {/* Art */}
@@ -68,9 +70,9 @@ export default function Home (): JSX.Element {
         enableFinana={false}
         enableFolding
         enableArt
-        heading='Art To LazuLight'
-        page='home'
+        heading="Art To LazuLight"
+        page="home"
       />
     </div>
-  )
+  );
 }
