208d3af7a607a3bb3361f2ebb2d47506407e5797
Sant268 <kunalbooch@hotmail.com>
2022-05-15 13:00:56 +0530

    lint fix

--- src/pages/DiamondCityLights.tsx
+++ src/pages/DiamondCityLights.tsx
@@ -120,7 +120,7 @@ function DiamondCityLightsCredits (): JSX.Element {
         <li><p><a href='https://twitter.com/snowharasho'>Squish</a></p></li>
         <li><p><a href='https://twitter.com/Arqodeon'>arqo</a></p></li>
       </ul>
-       <h3>Credits BGM</h3>
+      <h3>Credits BGM</h3>
       <ul>
         <li><p><a href='https://twitter.com/SonicFan53alt'>SonicFan53</a></p></li>
       </ul>
