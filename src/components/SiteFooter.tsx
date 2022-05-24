e3d81ed9366299e724665cfc4a9ca5366ba13291
Brandon Johns <69143173+Brandon-Johns@users.noreply.github.com>
2022-05-24 23:52:44 +1000

    general external links

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -17,7 +17,7 @@ export default function SiteFooter (props: Props): JSX.Element {
         <p>We thank the community for supporting the livers and each other.</p>
         <p>Finally, thank you to everyone who assisted in bringing these fan projects to life. Every contribution made this work even more special.</p>
         <h4>Disclaimer:</h4>
-        <p>This is a fan website, made by fans. We aimed to create this website in compliance with the <a href='https://event.nijisanji.app/guidelines/en/'>ANYCOLOR - Guidelines for Secondary Creation</a>. If there are any concerns, please contact one of our community’s discord moderators, as linked to on our <a href='/about'>about page</a>.</p>
+        <p>This is a fan website, made by fans. We aimed to create this website in compliance with the <a className='external-link-text' href='https://event.nijisanji.app/guidelines/en/'>ANYCOLOR - Guidelines for Secondary Creation</a>. If there are any concerns, please contact one of our community’s discord moderators, as linked to on our <a href='/about'>about page</a>.</p>
       </footer>
     </>
   )
