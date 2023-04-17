d272701138416a87f4cbae4be76b59ed4b7b8817
Brandon Johns <69143173+Brandon-Johns@users.noreply.github.com>
2023-04-17 15:50:03 +1000

    lint

--- src/year2/components/Timeline.tsx
+++ src/year2/components/Timeline.tsx
@@ -1,39 +1,39 @@
-import React from "react";
+import React from 'react'
 
-import classes from "./Timeline.module.scss";
+import classes from './Timeline.module.scss'
 
-type MemoryCommon = {
-  title: string;
-  member: "elira" | "finana" | "pomu";
-  date: Date;
-  submitterName: string;
-  submitterSocialUrl?: string;
-};
+interface MemoryCommon {
+  title: string
+  member: 'elira' | 'finana' | 'pomu'
+  date: Date
+  submitterName: string
+  submitterSocialUrl?: string
+}
 
-type YouTubeMemory = MemoryCommon & {
-  type: "youtube";
-  videoId: string;
-};
+type YouTubeMemoryType = MemoryCommon & {
+  type: 'youtube'
+  videoId: string
+}
 
-type TwitterMemory = MemoryCommon & {
-  type: "twitter";
-  tweetUrl: string;
-};
+type TwitterMemoryType = MemoryCommon & {
+  type: 'twitter'
+  tweetUrl: string
+}
 
-type MessageMemory = MemoryCommon & {
-  type: "message";
-  imgUrl?: string;
-  message: string;
-};
+type MessageMemoryType = MemoryCommon & {
+  type: 'message'
+  imgUrl?: string
+  message: string
+}
 
-type Memory = YouTubeMemory | TwitterMemory | MessageMemory;
+type MemoryType = YouTubeMemoryType | TwitterMemoryType | MessageMemoryType
 
-type TimelineProps = {
+interface TimelineProps {
   // Memories are ASSUMED TO BE IN ORDER by date
-  memories: Memory[];
-};
+  memories: MemoryType[]
+}
 
-export function Timeline({ memories }: TimelineProps) {
+export function Timeline ({ memories }: TimelineProps): JSX.Element {
   return (
     <div className={classes.timelineRoot}>
       <ul className={classes.timelineItems}>
@@ -42,76 +42,82 @@ export function Timeline({ memories }: TimelineProps) {
         ))}
       </ul>
     </div>
-  );
+  )
 }
 
-type TimelineItemProps = {
-  memory: Memory;
-};
+interface TimelineItemProps {
+  memory: MemoryType
+}
 
-function TimelineItem({ memory }: TimelineItemProps) {
+function TimelineItem ({ memory }: TimelineItemProps): JSX.Element {
   return (
     <li
       className={
-        memory.type === "youtube" || memory.type === "twitter"
+        memory.type === 'youtube' || memory.type === 'twitter'
           ? `${classes.item} ${classes.withEmbed} hi`
           : classes.item
       }
       style={
         {
-          "--stem-color": `${
-            memory.member === "elira"
-              ? "#fd83f2"
-              : memory.member === "finana"
-              ? "#a8f7f4"
-              : "#ffd889"
-          }`,
+          '--stem-color': `${
+            memory.member === 'elira'
+              ? '#fd83f2'
+              : memory.member === 'finana'
+              ? '#a8f7f4'
+              : '#ffd889'
+          }`
         } as React.CSSProperties
       }
     >
       <div className={classes.itemTitle}>
-        {memory.title} &bull;{" "}
-        {memory.submitterSocialUrl ? (
-          <a href={memory.submitterSocialUrl}>{memory.submitterName}</a>
-        ) : (
-          memory.submitterName
-        )}
+        {memory.title} &bull;{' '}
+        {memory.submitterSocialUrl !== undefined && memory.submitterSocialUrl !== ''
+          ? (
+            <a href={memory.submitterSocialUrl}>{memory.submitterName}</a>
+            )
+          : (
+              memory.submitterName
+            )}
       </div>
       <div className={classes.messageRow}>
         <div className={classes.itemDate}>
           <div className={classes.date}>{formatDate(memory.date)}</div>
         </div>
         <div className={classes.contentBox}>
-          {memory.type === "youtube" ? (
-            <YouTubeMemory memory={memory} />
-          ) : memory.type === "twitter" ? (
-            <TwitterMemory memory={memory} />
-          ) : (
-            <MessageMemory memory={memory} />
-          )}
+          {memory.type === 'youtube'
+            ? (
+              <YouTubeMemory memory={memory} />
+              )
+            : memory.type === 'twitter'
+              ? (
+                <TwitterMemory memory={memory} />
+                )
+              : (
+                <MessageMemory memory={memory} />
+                )}
         </div>
       </div>
     </li>
-  );
+  )
 }
 
-function formatDate(date: Date) {
+function formatDate (date: Date): string {
   return `${
     [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
     ][date.getMonth()]
-  } ${date.getFullYear()}`;
+  } ${date.getFullYear()}`
 }
 
 /*
@@ -120,38 +126,38 @@ Components for rendering different memories
 ===========================================
 */
 
-function MessageMemory({ memory }: { memory: MessageMemory }) {
+function MessageMemory ({ memory }: { memory: MessageMemoryType }): JSX.Element {
   return (
     <>
-      {memory.imgUrl && <img src={memory.imgUrl} />}
-      {memory.message.split("\n").map((line, i) => (
+      {memory.imgUrl !== undefined && memory.imgUrl !== '' && <img src={memory.imgUrl} />}
+      {memory.message.split('\n').map((line, i) => (
         <p key={i}>{line}</p>
       ))}
     </>
-  );
+  )
 }
 
-function YouTubeMemory({ memory }: { memory: YouTubeMemory }) {
+function YouTubeMemory ({ memory }: { memory: YouTubeMemoryType }): JSX.Element {
   // TODO: dynamic sizing based on available screen width?
   return (
     <div className={classes.youtubeContainer}>
       <iframe
         className={classes.youtubeEmbed}
         src={`https://www.youtube.com/embed/${memory.videoId}`}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        title='YouTube video player'
+        frameBorder='0'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
         allowFullScreen
-      ></iframe>
+      />
     </div>
-  );
+  )
 }
 
-function TwitterMemory({ memory }: { memory: TwitterMemory }) {
+function TwitterMemory ({ memory }: { memory: TwitterMemoryType }): JSX.Element {
   // Defaults to approximately min width (250px) because there's no preview content.
   return (
-    <blockquote className="twitter-tweet">
-      <a href={memory.tweetUrl}></a>
+    <blockquote className='twitter-tweet'>
+      <a href={memory.tweetUrl} />
     </blockquote>
-  );
+  )
 }
--- src/year2/pages/Home.tsx
+++ src/year2/pages/Home.tsx
@@ -1,82 +1,79 @@
-import React from "react";
+import React from 'react'
 
-import { Timeline } from "../components/Timeline";
+import { Timeline } from '../components/Timeline'
 
-export default function Home(): JSX.Element {
+export default function Home (): JSX.Element {
   return (
     <div>
       <Timeline
         memories={[
           {
-            type: "message",
-            submitterName: "Jay",
-            member: "finana",
+            type: 'message',
+            submitterName: 'Jay',
+            member: 'finana',
             date: new Date(2021, 4, 15),
-            title: "Lazulight debut!!",
-            message: "Wow!!",
+            title: 'Lazulight debut!!',
+            message: 'Wow!!'
           },
           {
-            type: "message",
-            submitterName: "Jay",
-            submitterSocialUrl: "speculative.tech",
-            member: "pomu",
+            type: 'message',
+            submitterName: 'Jay',
+            submitterSocialUrl: 'speculative.tech',
+            member: 'pomu',
             date: new Date(2021, 6, 1),
             title:
-              "What if I became Pomu but I had a really really really really really really really long title that wrapped around to a second line",
+              'What if I became Pomu but I had a really really really really really really really long title that wrapped around to a second line',
             message:
-              "I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! \
-              I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! \
-              I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! \
-              I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu!",
+              "I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu!  I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu! I'm Pomu!"
           },
           {
-            type: "message",
-            submitterName: "Jay",
-            member: "pomu",
+            type: 'message',
+            submitterName: 'Jay',
+            member: 'pomu',
             date: new Date(2021, 6, 14),
-            title: "minecraft with elira",
+            title: 'minecraft with elira',
             imgUrl:
-              "https://cdn.donmai.us/sample/b1/b0/__elira_pendora_and_pomu_rainpuff_nijisanji_and_1_more_drawn_by_walfie__sample-b1b07283875ba1f9f639794e2b1c93be.jpg",
+              'https://cdn.donmai.us/sample/b1/b0/__elira_pendora_and_pomu_rainpuff_nijisanji_and_1_more_drawn_by_walfie__sample-b1b07283875ba1f9f639794e2b1c93be.jpg',
             message:
-              "hey elira i'm begging you i'm on my knees here pleading please play minecraft with me tonight i am seriously begging here I am going to pass away please help minecraft is the only cure please elira",
+              "hey elira i'm begging you i'm on my knees here pleading please play minecraft with me tonight i am seriously begging here I am going to pass away please help minecraft is the only cure please elira"
           },
           {
-            type: "youtube",
-            submitterName: "Jay",
-            member: "finana",
+            type: 'youtube',
+            submitterName: 'Jay',
+            member: 'finana',
             date: new Date(2022, 0, 29),
-            title: "tsu-na-na-na-na-na-na-na-na-mi",
-            videoId: "3i_TLQsaAgU",
+            title: 'tsu-na-na-na-na-na-na-na-na-mi',
+            videoId: '3i_TLQsaAgU'
           },
           {
-            type: "youtube",
-            submitterName: "Jay",
-            member: "elira",
+            type: 'youtube',
+            submitterName: 'Jay',
+            member: 'elira',
             date: new Date(2022, 4, 15),
             title: "Elira's life-changing Shoujo Rei cover",
-            videoId: "DSSAL2b1VR4",
+            videoId: 'DSSAL2b1VR4'
           },
           {
-            type: "twitter",
-            submitterName: "Luca",
-            member: "elira",
+            type: 'twitter',
+            submitterName: 'Luca',
+            member: 'elira',
             date: new Date(2022, 5, 15),
-            title: "awesome",
+            title: 'awesome',
             tweetUrl:
-              "https://twitter.com/luca_kaneshiro/status/1536874758729437184",
+              'https://twitter.com/luca_kaneshiro/status/1536874758729437184'
           },
           {
-            type: "twitter",
-            submitterName: "oimo",
-            submitterSocialUrl: "https://twitter.com/oimo_0imo/",
-            member: "elira",
+            type: 'twitter',
+            submitterName: 'oimo',
+            submitterSocialUrl: 'https://twitter.com/oimo_0imo/',
+            member: 'elira',
             date: new Date(2023, 3, 15),
-            title: "UWOOOOOOOOOOOOOGGGGGHHHHH",
+            title: 'UWOOOOOOOOOOOOOGGGGGHHHHH',
             tweetUrl:
-              "https://twitter.com/oimo_0imo/status/1647099060082597888",
-          },
+              'https://twitter.com/oimo_0imo/status/1647099060082597888'
+          }
         ]}
       />
     </div>
-  );
+  )
 }
