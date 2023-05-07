64ded906e41a3f63a336c055cd3823869e7c7e39
ava8tor <mikeraymond211@gmail.com>
2023-05-07 16:29:38 -0400

    Modified Elira.tsx to add year1 art for example

--- src/year2/pages/Elira.tsx
+++ src/year2/pages/Elira.tsx
@@ -14,6 +14,7 @@ export default function Elira (): JSX.Element {
   // </div>
 
     <>
+      {/* Messages */}
       <MessageBoxesLayout
         titleType={firstNameLower}
         heading={`Messages To ${firstName}`}
@@ -21,6 +22,16 @@ export default function Elira (): JSX.Element {
         enableFolding
       >
         <MessageBoxesWithFlags messages={ImportMessages({ enableElira: true })} />
+      </MessageBoxesLayout>
+
+      {/* Art */}
+      <MessageBoxesLayout
+        titleType={firstNameLower}
+        heading={`Art To ${firstName}`}
+        page={firstNameLower}
+        enableFolding
+      >
+        <MessageBoxesWithFlags messages={ImportMessages({ enableElira: true, enableArt: true })} />
       </MessageBoxesLayout>
 
     </>
