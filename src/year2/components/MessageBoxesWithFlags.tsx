1b322bb7648a16d67f31612692cde0b9233fc190
ava8tor <mikeraymond211@gmail.com>
2023-05-11 17:36:41 -0400

    Cleaned the TalentLayout.scss file
    Made it to a module and renamed it to MessageBoxesWithFlags.scss

--- src/year2/components/MessageBoxesWithFlags.tsx
+++ src/year2/components/MessageBoxesWithFlags.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Image from '../../year1/components/Image'
 import { MessageForFlags } from '../../year2/components/ImportMessages'
-import './TalentLayout.scss'
+//import '../../year1/components/TalentLayout.scss'
+import classes from './MessageBoxesWithFlags.module.scss'
 
 export default function MessageBoxesWithFlags ({
   messages
@@ -11,14 +12,14 @@ export default function MessageBoxesWithFlags ({
   return (
     <>
       {messages.map((message, idx) => (
-        <div key={'msg' + idx.toString()} className='fan-submissions-submission'>
-          <Image className='fan-submissions-poststamp' src={message.countryFlag} />
-          <div className={`fan-submissions-message ${message.border}-border`}>
-            <h4 className='text-lg'>
+        <div key={'msg' + idx.toString()} className={`${classes[`fan-submissions-submission`]}`}>
+          <Image className={`${classes[`fan-submissions-poststamp`]}`} src={message.flag} />
+          <div className={`${classes[`fan-submissions-message`]} ${classes[`${message.border}-border`]}`}>
+            <h4 className="text-lg">
               {message.social_url === '' ? (message.name) : (<a href={message.social_url}>{message.name}</a>)}
             </h4>
             {message.msg !== '' && <p>{message.msg}</p>}
-            {message.art !== '' && (<Image className='fan-submissions-fanarti' src={message.art} alt='' enableZoom />)}
+            {message.art !== '' && (<Image className={`${classes['fan-submissions-fanarti']}`} src={message.art} alt='' enableZoom />)}
           </div>
         </div>
       ))}
