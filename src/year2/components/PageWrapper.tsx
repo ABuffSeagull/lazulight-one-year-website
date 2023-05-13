f78164c22cecbbe2d9fee788f42b48d7ae792d23
Brandon Johns <69143173+Brandon-Johns@users.noreply.github.com>
2023-05-13 14:00:31 +1000

    lint

--- src/year2/components/PageWrapper.tsx
+++ src/year2/components/PageWrapper.tsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import SiteHeader from './SiteHeader'
 import SiteFooter from './SiteFooter'
 import pw from './PageWrapper.module.scss'
-import pomu  from "../assets/background/Space-Pomu.png"
-import elira from "../assets/background/Space-Elira.png"
+import pomu from '../assets/background/Space-Pomu.png'
+import elira from '../assets/background/Space-Elira.png'
 import finana from '../assets/background/Space-Finana.png'
 
 export function PageWrapper ({
@@ -13,45 +13,37 @@ export function PageWrapper ({
   children: React.ReactNode
   pageTheme: string
 }): JSX.Element {
-  const [background, setBackground] = useState("page-bg-image");
-  var spacesuit
+  const [background, setBackground] = useState('page-bg-image')
+  let spacesuit
 
-  if(pageTheme == "elira"){
+  if (pageTheme == 'elira') {
     spacesuit = elira
-  } else if( pageTheme == "finana"){
+  } else if (pageTheme == 'finana') {
     spacesuit = finana
-  } else if(pageTheme == "pomu"){
+  } else if (pageTheme == 'pomu') {
     spacesuit = pomu
   }
 
   window.addEventListener('scroll', () => {
-    if(window.scrollY > 250){
-      
-      setBackground("page-bg-imagebg")
-      
-    } else{
-      setBackground("page-bg-image")
+    if (window.scrollY > 250) {
+      setBackground('page-bg-imagebg')
+    } else {
+      setBackground('page-bg-image')
     }
   })
 
-
-  
-  
-
-  
   return (
-    <div className={`  ${pw[`page-container}`]} `} >
-        <SiteHeader />
-        <img src = {spacesuit} className={ `  ${pw[background]} `}/> 
-        <div className ={ `  ${pw[`page-box`]} ` }> </div> 
-        {/* This is the body of the page */}
-        <div>
-          {children}
-        </div>
+    <div className={`  ${pw['page-container}']} `}>
+      <SiteHeader />
+      <img src={spacesuit} className={`  ${pw[background]} `} />
+      <div className={`  ${pw['page-box']} `}> </div>
+      {/* This is the body of the page */}
+      <div>
+        {children}
+      </div>
 
-        <SiteFooter theme='lazulight' />
-        </div>
-    
+      <SiteFooter theme='lazulight' />
+    </div>
+
   )
 }
-
