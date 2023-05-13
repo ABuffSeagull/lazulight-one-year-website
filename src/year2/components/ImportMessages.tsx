c8e2eac18af09d847a1e93d620cba820ed258a43
ava8tor <mikeraymond211@gmail.com>
2023-05-13 03:50:32 -0400

    Modified ImportMessages.tsx for the year2 messages
    changed the extensions in art.json and messages.json for images to .webp

--- src/year2/components/ImportMessages.tsx
+++ src/year2/components/ImportMessages.tsx
@@ -1,27 +1,18 @@
-// import * as messageListImport from '../../year1/assets/messageList.json'
 import * as messageListImport from '../assets/messages.json'
 import * as artListImport from '../assets/art.json'
-import EliraCorner from '../../year1/assets/Corners/Elira-MsgCorner.webp'
-import FinanaCorner from '../../year1/assets/Corners/Finana-MsgCorner.webp'
-import LazulightCorner from '../../year1/assets/Corners/LazuLight-Corner.webp'
-import PomuCorner from '../../year1/assets/Corners/Pomu-MsgCorner.webp'
 
 interface MessageRaw {
   name: string
-  social_url?: string
+  social?: string
   flag?: string
-  art_lazulight?: string
-  art_elira?: string
-  art_pomu?: string
-  art_finana?: string
-  msg_lazulight?: string
-  msg_elira?: string
-  msg_pomu?: string
-  msg_finana?: string
-  art_weewa?: string
-  art_pomudachi?: string
-  art_ryuguard?: string
-  msg_dcl?: string
+  lazulight?: string
+  elira?: string
+  pomu?: string
+  finana?: string
+  card_lazulight?: string
+  card_elira?: string
+  card_pomu?: string
+  card_finana?: string
 }
 
 export interface Message {
@@ -31,13 +22,9 @@ export interface Message {
   art: string
 }
 
-export interface MessageForCornerBoxes extends Message {
-  corner: string
-  border: string
-}
-
-export interface MessageForFlags extends MessageForCornerBoxes {
+export interface MessageForFlags extends Message {
   flag: string
+  border: string
 }
 
 // Shuffle algorithm: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
@@ -52,30 +39,14 @@ function shuffleMessages (array: Message[]): boolean {
 
 function filterMessage (
   enable: boolean,
-  enableArt: boolean,
   msg: string,
-  artLiver: string,
-  artMascot: string
+  cardMsg: string
 ): boolean {
-  // Msg mode: only allow if user has not submitted art
-  // Art mode: allow if user has submitted art
-  const ArtIsAvailable: boolean = artLiver !== '' || artMascot !== ''
+  const fanCardAvailable: boolean = cardMsg !== ''
   return (
     enable &&
-    ((!enableArt && msg !== '' && !ArtIsAvailable) || (enableArt && ArtIsAvailable))
+    ((msg !== '' && !fanCardAvailable) || (fanCardAvailable))
   )
-}
-
-function chooseArt (
-  enableArt: boolean,
-  artLiver: string,
-  artMascot: string
-): string {
-  if (!enableArt) { return '' }
-  if (artLiver !== '') { return artLiver }
-  if (artMascot !== '') { return artMascot }
-  // Default for no art
-  return ''
 }
 
 // Extract all messages from JSON file, filter by enabled type, format, random shuffle, then output
@@ -94,123 +65,91 @@ export default function ImportMessages ({
   enableDcl?: boolean
   enableArt?: boolean
 }): MessageForFlags[] {
-  const messageListRaw = messageListImport.all as MessageRaw[]
+  // let messageListRaw; //= messageListImport.all as MessageRaw[]
 
-  console.log(messageListRaw);
+  const messageListRaw = !enableArt
+    ? messageListImport.all as MessageRaw[]
+    : artListImport.all as MessageRaw[]
+  // messageListRaw = messageListImport.all as MessageRaw[];
+
+  console.log(messageListRaw)
 
   const Messages: MessageForFlags[] = []
   messageListRaw.forEach((msg: MessageRaw): void => {
     // (Brandon): ForEach loop didn't work for some reason
-    if (msg.social_url === undefined) { msg.social_url = '' }
+    if (msg.social === undefined) { msg.social = '' }
     if (msg.flag === undefined) { msg.flag = '/anniversary2/stamps/Stamp-Unknown.webp' }
-    if (msg.art_lazulight === undefined) { msg.art_lazulight = '' }
-    if (msg.art_elira === undefined) { msg.art_elira = '' }
-    if (msg.art_pomu === undefined) { msg.art_pomu = '' }
-    if (msg.art_finana === undefined) { msg.art_finana = '' }
-    if (msg.msg_lazulight === undefined) { msg.msg_lazulight = '' }
-    if (msg.msg_elira === undefined) { msg.msg_elira = '' }
-    if (msg.msg_pomu === undefined) { msg.msg_pomu = '' }
-    if (msg.msg_finana === undefined) { msg.msg_finana = '' }
-    if (msg.art_weewa === undefined) { msg.art_weewa = '' }
-    if (msg.art_pomudachi === undefined) { msg.art_pomudachi = '' }
-    if (msg.art_ryuguard === undefined) { msg.art_ryuguard = '' }
-    if (msg.msg_dcl === undefined) { msg.msg_dcl = '' }
+    if (msg.lazulight === undefined) { msg.lazulight = '' }
+    if (msg.elira === undefined) { msg.elira = '' }
+    if (msg.pomu === undefined) { msg.pomu = '' }
+    if (msg.finana === undefined) { msg.finana = '' }
+    if (msg.card_lazulight === undefined) { msg.card_lazulight = '' }
+    if (msg.card_elira === undefined) { msg.card_elira = '' }
+    if (msg.card_pomu === undefined) { msg.card_pomu = '' }
+    if (msg.card_finana === undefined) { msg.card_finana = '' }
 
-    if (
-      filterMessage(
-        enableLazulight,
-        enableArt,
-        msg.msg_lazulight,
-        msg.art_lazulight,
-        ''
-      )
-    ) {
+    if (msg.flag === '') { msg.flag = '/anniversary2/stamps/Stamp-Unknown.webp' };
+
+    if (filterMessage(
+      enableLazulight,
+      msg.lazulight,
+      msg.card_lazulight
+    )) {
       Messages.push({
         name: msg.name,
         flag: msg.flag,
-        social_url: msg.social_url,
-        msg: msg.msg_lazulight,
-        art: enableArt ? msg.art_lazulight : '',
-        corner: LazulightCorner,
+        social_url: msg.social,
+        msg: msg.lazulight,
+        art: enableArt ? msg.lazulight : msg.card_lazulight,
         border: 'lazulight'
       })
     }
-    if (
-      filterMessage(
-        enableElira,
-        enableArt,
-        msg.msg_elira,
-        msg.art_elira,
-        msg.art_weewa
-      )
-    ) {
-      console.log("true")
+    if (filterMessage(
+      enableElira,
+      msg.elira,
+      msg.card_elira
+    )) {
       Messages.push({
         name: msg.name,
         flag: msg.flag,
-        social_url: msg.social_url,
-        msg: msg.msg_elira,
-        art: chooseArt(enableArt, msg.art_elira, msg.art_weewa),
-        corner: EliraCorner,
+        social_url: msg.social,
+        msg: msg.elira,
+        art: enableArt ? msg.elira : msg.card_elira,
         border: 'elira'
       })
     }
-    else {console.log("false")}
-    if (
-      filterMessage(
-        enablePomu,
-        enableArt,
-        msg.msg_pomu,
-        msg.art_pomu,
-        msg.art_pomudachi
-      )
-    ) {
+    if (filterMessage(
+      enablePomu,
+      msg.pomu,
+      msg.card_pomu
+    )) {
       Messages.push({
         name: msg.name,
         flag: msg.flag,
-        social_url: msg.social_url,
-        msg: msg.msg_pomu,
-        art: chooseArt(enableArt, msg.art_pomu, msg.art_pomudachi),
-        corner: PomuCorner,
+        social_url: msg.social,
+        msg: msg.pomu,
+        art: enableArt ? msg.pomu : msg.card_pomu,
         border: 'pomu'
       })
     }
-    if (
-      filterMessage(
-        enableFinana,
-        enableArt,
-        msg.msg_finana,
-        msg.art_finana,
-        msg.art_ryuguard
-      )
-    ) {
+    if (filterMessage(
+      enableFinana,
+      msg.finana,
+      msg.card_finana
+    )) {
       Messages.push({
         name: msg.name,
         flag: msg.flag,
-        social_url: msg.social_url,
-        msg: msg.msg_finana,
-        art: chooseArt(enableArt, msg.art_finana, msg.art_ryuguard),
-        corner: FinanaCorner,
+        social_url: msg.social,
+        msg: msg.finana,
+        art: enableArt ? msg.finana : msg.card_finana,
         border: 'finana'
-      })
-    }
-    if (
-      filterMessage(enableDcl, enableArt, msg.msg_dcl, '', '')
-    ) {
-      Messages.push({
-        name: msg.name,
-        flag: msg.flag,
-        social_url: msg.social_url,
-        msg: msg.msg_dcl,
-        art: '',
-        corner: LazulightCorner,
-        border: 'dcl'
       })
     }
   })
   shuffleMessages(Messages)
 
-  console.log("messages");
-  console.log(Messages);
+  console.log('messages')
+  console.log(Messages)
   return Messages
 }
--- src/year2/components/MessageBoxesWithFlags.tsx
+++ src/year2/components/MessageBoxesWithFlags.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from '../../year1/components/Image'
 import { MessageForFlags } from '../../year2/components/ImportMessages'
-//import '../../year1/components/TalentLayout.scss'
+// import '../../year1/components/TalentLayout.scss'
 import classes from './MessageBoxesWithFlags.module.scss'
 
 export default function MessageBoxesWithFlags ({
@@ -12,14 +12,14 @@ export default function MessageBoxesWithFlags ({
   return (
     <>
       {messages.map((message, idx) => (
-        <div key={'msg' + idx.toString()} className={`${classes[`fan-submissions-submission`]}`}>
-          <Image className={`${classes[`fan-submissions-poststamp`]}`} src={message.flag} />
-          <div className={`${classes[`fan-submissions-message`]} ${classes[`${message.border}-border`]}`}>
-            <h4 className="text-lg">
+        <div key={'msg' + idx.toString()} className={`${classes['fan-submissions-submission']}`}>
+          <Image className={`${classes['fan-submissions-poststamp']}`} src={message.flag} />
+          <div className={`${classes['fan-submissions-message']} ${classes[`${message.border}-border`]}`}>
+            <h4 className='text-lg'>
               {message.social_url === '' ? (message.name) : (<a href={message.social_url}>{message.name}</a>)}
             </h4>
-            {message.msg !== '' && <p>{message.msg}</p>}
-            {message.art !== '' && (<Image className={`${classes['fan-submissions-fanarti']}`} src={message.art} alt='' enableZoom />)}
+            {(message.msg !== '' && message.art === '') && <p>{message.msg}</p>}
+            {message.art !== '' && (<Image className={`${classes['fan-submissions-fanarti']}`} src={message.art} alt={message.msg} enableZoom />)}
           </div>
         </div>
       ))}
