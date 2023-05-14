f43b07d02c5e8041fdf1f7bf6750fba289083ef9
Jeff Tao <jatao88@gmail.com>
2023-05-13 22:15:45 -0400

    Lint pass

--- src/year2/components/DoujinViewer.tsx
+++ src/year2/components/DoujinViewer.tsx
@@ -1,29 +1,27 @@
 import React from 'react'
 import classes from './DoujinViewer.module.scss'
 
-const TOTAL_PAGES = 63;
+const TOTAL_PAGES = 63
 
-export default function DoujinViewer ({
-}: {
-}): JSX.Element {
+export default function DoujinViewer (): JSX.Element {
   const [doujinContainerSize, setDoujinContainerSize] = React.useState({ width: window.innerWidth, height: window.innerHeight })
   const doujinObserver = React.useRef<ResizeObserver>()
   const [displayMode, setDisplayMode] = React.useState('fit-height')
-  const doujinContainer = React.useRef<HTMLDivElement>(null);
+  const doujinContainer = React.useRef<HTMLDivElement>(null)
 
-  const [currentPage, setCurrentPage] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(0)
 
   const tryNextPage = React.useCallback(() => {
     if (currentPage < TOTAL_PAGES - 1) {
-        setCurrentPage(currentPage + 1);
-        doujinContainer.current?.scrollIntoView({ block: displayMode === "fit-height" ? "center" : "start" });
+      setCurrentPage(currentPage + 1)
+      doujinContainer.current?.scrollIntoView({ block: displayMode === 'fit-height' ? 'center' : 'start' })
     }
-  }, [currentPage, setCurrentPage, doujinContainer.current]);
+  }, [currentPage, setCurrentPage, doujinContainer.current])
 
   const tryPrevPage = React.useCallback(() => {
     if (currentPage > 0) {
-        setCurrentPage(currentPage - 1);
-        doujinContainer.current?.scrollIntoView({ block: displayMode === "fit-height" ? "center" : "start" });
+      setCurrentPage(currentPage - 1)
+      doujinContainer.current?.scrollIntoView({ block: displayMode === 'fit-height' ? 'center' : 'start' })
     }
   }, [currentPage, setCurrentPage, doujinContainer.current])
 
@@ -38,66 +36,74 @@ export default function DoujinViewer ({
       <h1>
         Lazulight Manga Anthology
       </h1>
-    <div className={classes['doujin-reader']} onKeyDown={(e) => {
-        if (e.key === "ArrowLeft") {
+      <div
+        className={classes['doujin-reader']} onKeyDown={(e) => {
+          if (e.key === 'ArrowLeft') {
             tryNextPage()
-        } else if (e.key === "ArrowRight") {
+          } else if (e.key === 'ArrowRight') {
             tryPrevPage()
-        }
-    }}>
+          }
+        }}
+      >
 
-    <div className={classes['doujin-container']} style={
-        displayMode === 'fit-width' ? {
-            height: doujinContainerSize.width * (3 / 2)
-        } : {
-            height: doujinContainerSize.height
-        }
-    } ref={doujinContainer}>
         <div
-            className={classes['doujin-size-detect']} ref={(node) => {
-            if (doujinObserver.current === undefined) {
-                doujinObserver.current = new ResizeObserver(([entry]) => {
-                console.log('Got width update', entry)
-                setDoujinContainerSize((currentDoujinContainerSize) => {
-                    const newWidth = entry.borderBoxSize[0].inlineSize;
-                    const newHeight = entry.borderBoxSize[0].blockSize;
-                    if (newWidth !== currentDoujinContainerSize.width || newHeight !== currentDoujinContainerSize.height) {
-                        return {
-                            width: newWidth,
-                            height: newHeight
-                        }
-                    } else {
-                        return currentDoujinContainerSize;
-                    }
-                })
-                })
+          className={classes['doujin-container']} style={
+        displayMode === 'fit-width'
+          ? {
+              height: doujinContainerSize.width * (3 / 2)
             }
-            if (node != null) {
-                doujinObserver.current?.observe(node)
+          : {
+              height: doujinContainerSize.height
             }
-            }}
+    } ref={doujinContainer}
         >
-        </div>
-        <button className={classes['next-page']} onClick={tryNextPage} disabled={currentPage === TOTAL_PAGES - 1}></button>
-        <button className={classes['prev-page']} onClick={tryPrevPage} disabled={currentPage === 0}></button>
-        {
-            [...Array(TOTAL_PAGES).keys()].map((page) => 
-            (Math.abs(page - currentPage) <= 1) ?  <img
-                key={page}
-                src={`/anniversary2/doujin/image-${page.toString().padStart(3, "0")}.jpg`}
-                className={page === currentPage ? undefined : classes['hidden-page']}
-                style={{
-                    width: displayMode === "fit-width" ? doujinContainerSize.width : undefined,
-                    height: displayMode === "fit-height" ? doujinContainerSize.height : undefined
-                }} /> : null
+          <div
+            className={classes['doujin-size-detect']} ref={(node) => {
+              if (doujinObserver.current === undefined) {
+                doujinObserver.current = new ResizeObserver(([entry]) => {
+                  console.log('Got width update', entry)
+                  setDoujinContainerSize((currentDoujinContainerSize) => {
+                    const newWidth = entry.borderBoxSize[0].inlineSize
+                    const newHeight = entry.borderBoxSize[0].blockSize
+                    if (newWidth !== currentDoujinContainerSize.width || newHeight !== currentDoujinContainerSize.height) {
+                      return {
+                        width: newWidth,
+                        height: newHeight
+                      }
+                    } else {
+                      return currentDoujinContainerSize
+                    }
+                  })
+                })
+              }
+              if (node != null) {
+                doujinObserver.current?.observe(node)
+              }
+            }}
+          />
+          <button className={classes['next-page']} onClick={tryNextPage} disabled={currentPage === TOTAL_PAGES - 1} />
+          <button className={classes['prev-page']} onClick={tryPrevPage} disabled={currentPage === 0} />
+          {
+            [...Array(TOTAL_PAGES).keys()].map((page) =>
+              (Math.abs(page - currentPage) <= 1)
+                ? <img
+                    key={page}
+                    src={`/anniversary2/doujin/image-${page.toString().padStart(3, '0')}.jpg`}
+                    className={page === currentPage ? undefined : classes['hidden-page']}
+                    style={{
+                      width: displayMode === 'fit-width' ? doujinContainerSize.width : undefined,
+                      height: displayMode === 'fit-height' ? doujinContainerSize.height : undefined
+                    }}
+                  />
+                : null
             )
         }
-    </div>
-    <div className={classes['bottom-nav']}>
-        <div className={classes['page-count']}>{currentPage + 1} / {TOTAL_PAGES}</div>
-        <button className={classes['change-mode']} onClick={() => displayMode === 'fit-height' ? setDisplayMode('fit-width') : setDisplayMode('fit-height')}>Fit {displayMode === 'fit-width' ? <>Width</> : <>Height</>}</button>
-    </div>
-    </div>
+        </div>
+        <div className={classes['bottom-nav']}>
+          <div className={classes['page-count']}>{currentPage + 1} / {TOTAL_PAGES}</div>
+          <button className={classes['change-mode']} onClick={() => displayMode === 'fit-height' ? setDisplayMode('fit-width') : setDisplayMode('fit-height')}>Fit {displayMode === 'fit-width' ? <>Width</> : <>Height</>}</button>
+        </div>
+      </div>
     </>
   )
 }
