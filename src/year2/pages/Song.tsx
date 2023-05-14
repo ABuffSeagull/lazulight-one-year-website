4e0fcbbd0555db056d563d8c8009ae284041d62b
Austinx88 <44851962+Austinx88@users.noreply.github.com>
2023-05-14 02:17:23 -0400

    Song page timer

--- src/year2/pages/Song.tsx
+++ src/year2/pages/Song.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import Countdown, { CountdownRenderProps } from 'react-countdown'
+
+const countdownRenderer = ({
+  days,
+  hours,
+  minutes,
+  seconds,
+  completed
+}: CountdownRenderProps): JSX.Element => {
+  if (completed) {
+    return <>Done</>
+  }
+  // Render countdown
+  return (
+
+    <span> {hours}:{minutes}:{seconds}</span>
+
+  )
+}
+
+const releaseDate = '2023-05-16T12:12:00+09:00'
 
 export default function Song (): JSX.Element {
   return (
-    <div>
-      test
+    <div style={{ fontSize: '300px', color: 'white' }}>
+      <Countdown date={releaseDate} renderer={countdownRenderer} />
     </div>
   )
 }
