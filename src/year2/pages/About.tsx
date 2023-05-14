3727716644ccde393931e8f7f4f71e71460922c8
Austinx88 <44851962+Austinx88@users.noreply.github.com>
2023-05-14 02:54:34 -0400

    Base for

--- src/year2/pages/About.tsx
+++ src/year2/pages/About.tsx
@@ -7,9 +7,24 @@ export default function About (): JSX.Element {
     <div className='about-container'>
       <Textbox>
         <h2>About</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio rerum quas nam harum cumque architecto nostrum voluptatibus, voluptate maxime consequatur ea consectetur fugit alias nemo, error doloremque fugiat ut atque.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum voluptatem doloremque, sequi a minus architecto voluptates facilis vitae dicta dolorem nam, voluptatum numquam blanditiis saepe necessitatibus, sapiente id ratione soluta?</p>
+        <p>This fan website showcases our thanks and congratulations to Elira, Pomu, and Finana (LazuLight) of Nijisanji EN for all their content from 2 years of streaming.</p>
+        <p>Fans of LazuLight from across the world and from all walks of life joined together to create this expression of thanks. Every showcased message, art, music, game, and this website itself was created specifically for this purpose. This project is the culmination of over 4 months of efforts from artists, programmers, writers, and many others.</p>
+        <p>The fruits of this labor include this website, a doujin and a song. Thank you to the LazuLight fan discords, contributors, and all the fans for your hard work and support.</p>
+        <h3>Fan Discord Links</h3>
+        <p><a className='external-link-text' href='https://discord.gg/2434club'>Nijisanji EN Fanserver</a></p>
+        <p><a className='external-link-text' href='https://discord.gg/pomu'>Pomucord</a></p>
+        <p><a className='external-link-text' href='https://discord.gg/gWUFJS2JtF'>Elicord</a></p>
+        <p><a className='external-link-text' href='https://discord.gg/425bxuSsff'>Finana Finclub</a></p>
+      </Textbox>
+    
+      <Textbox>
+        <h2>Credits</h2>
+        
+
+
       </Textbox>
     </div>
   )
 }
+
+
