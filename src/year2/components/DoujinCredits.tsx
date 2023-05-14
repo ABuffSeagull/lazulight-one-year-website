5ed8fb725aafdd88e95a45736d1a2fa02545f654
ava8tor <mikeraymond211@gmail.com>
2023-05-14 03:54:52 -0400

    Cleaned up the credit list for mobile view

--- src/year2/components/DoujinCredits.tsx
+++ src/year2/components/DoujinCredits.tsx
@@ -24,82 +24,83 @@ export default function DoujinCredits (): JSX.Element {
         </ul>
 
         <h3>Pomu: Do You Think Love Can Bloom Even at a Maid Cafe?</h3>
-        <ul>
+        {/* <ul>
           <li><h4>Cover Artist</h4></li>
           <li><h4>Manga Artist</h4></li>
           <li><h4>Writer</h4></li>
-        </ul>
+        </ul> */}
+        <h4>Cover Artist</h4>
         <ul>
-          <li>
-            <p><a href='https://twitter.com/parkiranhonda'>parkiranhonda</a></p>
-          </li>
-          <li>
-            <p><a href='https://twitter.com/OwenAnaken'>Ago</a></p>
-          </li>
-          <li>
-            <p><a href='https://twitter.com/ZephyrKitten'>Zephyr Kitten</a></p>
-          </li>
+          <li><p><a href='https://twitter.com/parkiranhonda'>parkiranhonda</a></p></li>
+        </ul>
+        <h4>Manga Artist</h4>
+        <ul>
+          <li><p><a href='https://twitter.com/OwenAnaken'>Ago</a></p></li>
+        </ul>
+        <h4>Writer</h4>
+        <ul>
+          <li><p><a href='https://twitter.com/ZephyrKitten'>Zephyr Kitten</a></p></li>
         </ul>
 
         <h3>Elira: Love at First Light</h3>
+        <h4>Cover Artist</h4>
         <ul>
-          <li><h4>Cover Artist</h4></li>
-          <li><h4>Manga Artist</h4></li>
-          <li><h4>Writer</h4></li>
+          <li><p><a href='https://www.instagram.com/ann_teasocial/'>Ann_TeaSocial</a></p></li>
         </ul>
+        <h4>Manga Artist</h4>
         <ul>
-          <li>
-            <p><a href='https://www.instagram.com/ann_teasocial/'>Ann_TeaSocial</a></p>
-          </li>
-          <li>
-            <p><a href='https://twitter.com/r0ckybirdy'>RockyBirdy</a></p>
-          </li>
-          <li>
-            <p><a href='https://twitter.com/coldace_24'>Zephyr Monochrome</a></p>
-          </li>
+          <li><p><a href='https://twitter.com/r0ckybirdy'>RockyBirdy</a></p></li>
+        </ul>
+        <h4>Writer</h4>
+        <ul>
+          <li><p><a href='https://twitter.com/coldace_24'>Zephyr Monochrome</a></p></li>
         </ul>
 
         <h3>Finana: Laundry Day</h3>
-        <ul>
-          <li><h4>Cover Artist</h4></li>
-          <li><h4>Manga Artist, Writer</h4></li>
-          <li><h4>Writer</h4></li>
-        </ul>
+        <h4>Cover Artist</h4>
         <ul>
           <li><p><a href='https://twitter.com/sirohappyend'>Siro</a></p></li>
+        </ul>
+        <h4>Manga Artist, Writer</h4>
+        <ul>
           <li><p><a href='https://twitter.com/MizMillificent'>Miz Millificent</a></p></li>
+        </ul>
+        <h4>Writer</h4>
+        <ul>
           <li><p><a href='https://twitter.com/CrimsonSilver4'>CrimsonSilver4</a></p></li>
         </ul>
 
         <h3>Scenario 1: Blind Date</h3>
-        <ul>
-          <li><h4>Cover Artist</h4></li>
-          <li><h4>Manga Artist</h4></li>
-          <li><h4>Writer</h4></li>
-        </ul>
+        <h4>Cover Artist</h4>
         <ul>
           <li><p><a href='https://twitter.com/amechiame'>amechi</a></p></li>
+        </ul>
+        <h4>Manga Artist</h4>
+        <ul>
           <li><p><a href='https://twitter.com/Aria404_'>Aria404</a></p></li>
+        </ul>
+        <h4>Writer</h4>
+        <ul>
           <li><p><a href='https://twitter.com/wildnexus'>wildnexus</a></p></li>
         </ul>
 
         <h3>Scenario 2: Lazulight's Last Day Together</h3>
-        <ul>
-          <li><h4>Cover Artist, Manga Artist, Writer</h4></li>
-          <li><h4>Writer</h4></li>
-        </ul>
+        <h4>Cover Artist, Manga Artist, Writer</h4>
         <ul>
           <li><p><a href='https://twitter.com/MelonbreadFBP'>Melonbreadfbp</a></p></li>
+        </ul>
+        <h4>Writer</h4>
+        <ul>
           <li><p><a href='https://twitter.com/JF__ND'>JFMD</a></p></li>
         </ul>
 
         <h3>Scenario 3: Our Treasure</h3>
-        <ul>
-          <li><h4>Cover Artist, Manga Artist</h4></li>
-          <li><h4>Writer</h4></li>
-        </ul>
+        <h4>Cover Artist, Manga Artist</h4>
         <ul>
           <li><p><a href='https://twitter.com/tsukinaga_b'>tsukinaga_b</a></p></li>
+        </ul>
+        <h4>Writer</h4>
+        <ul>
           <li><p><a href='https://twitter.com/wildnexus'>wildnexus</a></p></li>
         </ul>
 
@@ -120,12 +121,12 @@ export default function DoujinCredits (): JSX.Element {
           <li><p><a href='https://twitter.com/beesDotMoe'>Lilybees</a></p></li>
         </ul>
         <h3>Website Doujin Icon</h3>
-        <ul>
-          <li><h4>Designed by</h4></li>
-          <li><h4>Edited and Colored by</h4></li>
-        </ul>
+        <h4>Designed by</h4>
         <ul>
           <li><p><a href='https://twitter.com/tsukinaga_b'>tsukinaga_b</a></p></li>
+        </ul>
+        <h4>Edited and Colored by</h4>
+        <ul>
           <li><p><a href='https://twitter.com/wildnexus'>wildnexus</a></p></li>
         </ul>
       </div>
