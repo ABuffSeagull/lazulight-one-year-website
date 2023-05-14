01a7a972d14b52ba2ff896122e5ddf9eac7f8512
Jeff Tao <jatao88@gmail.com>
2023-05-13 21:33:23 -0400

    Ditch PDF, use individual page images

--- src/year2/components/DoujinViewer.tsx
+++ src/year2/components/DoujinViewer.tsx
@@ -1,50 +1,16 @@
 import React from 'react'
-import { Document, Page } from 'react-pdf/dist/esm/entry.vite'
-
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
 import classes from './DoujinViewer.module.scss'
 
-// https://usehooks.com/useWindowSize/
-function useWindowSize () {
-  // Initialize state with undefined width/height so server and client renders match
-  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-  const [windowSize, setWindowSize] = React.useState<{ width?: number, height?: number }>({
-    width: undefined,
-    height: undefined
-  })
-
-  React.useEffect(() => {
-    // Handler to call on window resize
-    function handleResize () {
-      // Set window width/height to state
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight
-      })
-    }
-
-    // Add event listener
-    window.addEventListener('resize', handleResize)
-
-    // Call handler right away so state gets updated with initial window size
-    handleResize()
-
-    // Remove event listener on cleanup
-    return () => window.removeEventListener('resize', handleResize)
-  }, []) // Empty array ensures that effect is only run on mount
-
-  return windowSize
-}
+const TOTAL_PAGES = 63;
 
 export default function DoujinViewer ({
 }: {
 }): JSX.Element {
-  const { height: windowHeight } = useWindowSize()
   const [doujinContainerSize, setDoujinContainerSize] = React.useState({ width: window.innerWidth, height: window.innerHeight })
   const doujinObserver = React.useRef<ResizeObserver>()
   const [displayMode, setDisplayMode] = React.useState('fit-height')
+  const doujinContainer = React.useRef<HTMLDivElement>(null);
 
-  const [totalPages, setTotalPages] = React.useState(53);
   const [currentPage, setCurrentPage] = React.useState(0);
 
   React.useEffect(() => {
@@ -65,7 +31,7 @@ export default function DoujinViewer ({
         } : {
             height: doujinContainerSize.height
         }
-    }>
+    } ref={doujinContainer}>
         <div
             className={classes['doujin-size-detect']} ref={(node) => {
             if (doujinObserver.current === undefined) {
@@ -91,30 +57,20 @@ export default function DoujinViewer ({
             }}
         >
         </div>
-        <button className={classes['prev-page']} onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === totalPages - 1}></button>
-        <button className={classes['next-page']} onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 0}></button>
-        <Document file='/anniversary2/doujin/Lazulight Anthology Vol. 1.pdf' onLoadSuccess={(document) => {
-            setTotalPages(document.numPages)
-        }}>
-            {
-                [...Array(totalPages).keys()].map((page) => 
-                // Rendering each page has an annoying delay because react-pdf is rendering the PDF page to an image in a worker
-                // then serializing it and sending it to the JS thread.
-                // 
-                // To improve performance, we virtualize the list of pages: only render pages within 1 of the current page.
-                // This is to prevent huge initial load time (trying to render all pages) while also minimizing page turn delay
-                // which shows as an annoying blank black page while the render happens.
-                //
-                // If you skip forward by a bunch of pages (mash next page), having some render delay is unfortunately inevitable.
-                // Virtualization is based on the assumption that the user will spend at least a little time on each page.
-                (Math.abs(page - currentPage) <= 1) ?  <Page
-                    key={page} pageIndex={page}
-                    className={page === currentPage ? undefined : classes['hidden-page']}
-                    width={displayMode === "fit-width" ? doujinContainerSize.width : undefined}
-                    height={displayMode === "fit-height" ? doujinContainerSize.height : undefined} /> : null
-                )
-            }
-        </Document>
+        <button className={classes['prev-page']} onClick={() => { setCurrentPage(currentPage + 1); doujinContainer.current?.scrollIntoView({ block: "center"})}} disabled={currentPage === TOTAL_PAGES - 1}></button>
+        <button className={classes['next-page']} onClick={() => { setCurrentPage(currentPage - 1); doujinContainer.current?.scrollIntoView({ block: "center"})}} disabled={currentPage === 0}></button>
+        {
+            [...Array(TOTAL_PAGES).keys()].map((page) => 
+            (Math.abs(page - currentPage) <= 1) ?  <img
+                key={page}
+                src={`/anniversary2/doujin/image-${page.toString().padStart(3, "0")}.jpg`}
+                className={page === currentPage ? undefined : classes['hidden-page']}
+                style={{
+                    width: displayMode === "fit-width" ? doujinContainerSize.width : undefined,
+                    height: displayMode === "fit-height" ? doujinContainerSize.height : undefined
+                }} /> : null
+            )
+        }
     </div>
     </>
   )
