f292fb352e3d6727bdacd2d90a58b04762197af6
Brandon Johns <69143173+Brandon-Johns@users.noreply.github.com>
2023-05-15 22:26:57 +1000

    footer text - more or less copy of year 1

--- src/year2/components/SiteFooter.tsx
+++ src/year2/components/SiteFooter.tsx
@@ -10,11 +10,11 @@ export default function SiteFooter ({
     <>
       <footer className={`${classes.footer} ${classes[`${theme}-footer-border`]}`}>
         <h4>About this website</h4>
-        <p />
-
+        <p>This fan website showcases our thanks and congratulations to LazuLight of Nijisanji EN on their second full year of streaming. Every showcased message, art, music, and game was created specifically for this anniversary event.</p>
         <h4>Thanks:</h4>
-        <p />
-
+        <p>We would like to thank Pomu, Elira, and Finana, for the joy you have brought to us all. Furthermore, we thank the Nijisanji staff for all of your efforts behind the scenes to enable the livers in their work.</p>
+        <p>We thank the community for supporting the livers and each other.</p>
+        <p>Finally, thank you to everyone who assisted in bringing these fan projects to life. Every contribution made this work even more special.</p>
         <h4>Disclaimer:</h4>
         <p>This is a fan website, made by fans. We aimed to create this website in compliance with the <a className='external-link-text' href='https://event.nijisanji.app/guidelines/en/'>ANYCOLOR - Guidelines for Secondary Creation</a>. If there are any concerns or enquiries, please contact us by email at <a href='mailto:lazuprojectsteam@gmail.com'>lazuprojectsteam@gmail.com</a>.</p>
       </footer>
