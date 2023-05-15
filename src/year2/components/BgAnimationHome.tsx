b7184a9c0d32ae165d0bb5ce2defcc2d1f5b6379
Brandon Johns <69143173+Brandon-Johns@users.noreply.github.com>
2023-05-15 19:47:19 +1000

    home bg - pomudachi weewa

--- src/year2/components/BgAnimationHome.tsx
+++ src/year2/components/BgAnimationHome.tsx
@@ -4,6 +4,8 @@ import pw from './PageWrapper.module.scss'
 import lazuElira from '../assets/background/LZL-Elira2-fade.webp'
 import lazuPomu from '../assets/background/LZL-Pomu-fade.webp'
 import lazuFinana from '../assets/background/LZL-Finana-fade.webp'
+import pomudachiBalloon from '../assets/background/LZL-Pomudachi-Balloon.webp'
+import weewaBalloon from '../assets/background/LZL-Weewas-Balloon.webp'
 
 export default function BgAnimationFinana (): JSX.Element {
   return (
@@ -11,6 +13,8 @@ export default function BgAnimationFinana (): JSX.Element {
       <Image src={lazuElira} className={pw['home-bg-animation-elira']} />
       <Image src={lazuPomu} className={pw['home-bg-animation-pomu']} />
       <Image src={lazuFinana} className={pw['home-bg-animation-finana']} />
+      <Image src={pomudachiBalloon} className={pw['home-bg-animation-pomudachi-b']} />
+      <Image src={weewaBalloon} className={pw['home-bg-animation-weewa-b']} />
     </>
   )
 }
