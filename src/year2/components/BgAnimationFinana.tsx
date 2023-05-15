d6e0bb70ea08166c4fd8ddbdab9d0900eb58e3b8
Austinx88 <44851962+Austinx88@users.noreply.github.com>
2023-05-14 22:51:19 -0400

    Finana items

--- src/year2/components/BgAnimationFinana.tsx
+++ src/year2/components/BgAnimationFinana.tsx
@@ -2,11 +2,19 @@ import Image from '../../year1/components/Image'
 import pw from './PageWrapper.module.scss'
 
 import spaceFinana from '../assets/background/Space-Finana.webp'
+import animate1 from '../assets/background/Finana-egg.webp'
+import animate2 from '../assets/background/Finana-toothbrush.webp'
+import animate3 from '../assets/background/Finana-helmet.webp'
 
 export default function BgAnimationFinana (): JSX.Element {
   return (
     <>
       <Image src={spaceFinana} className={pw['page-bg-animation-liver']} />
+      <Image src={animate1} className={pw['page-bg-animation-finana-a']} />
+      <Image src={animate2} className={pw['page-bg-animation-finana-b']} />
+      <Image src={animate3} className={pw['page-bg-animation-finana-c']} />
+      
+      
     </>
   )
 }
